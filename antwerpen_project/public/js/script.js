8bae53dce58b39fcf9f46cfed9ee7566e6eb6d7f
hasan <hasan.gurbuz@student.kdg.be>
2016-04-26 21:11:56 +0200

    controle voor locaties

--- antwerpen_project/public/js/script.js
+++ antwerpen_project/public/js/script.js
@@ -47,49 +47,54 @@ $(document).ready(function () {
 		  
 		  
 		  $.ajax({url: root+"/kaart/api/get_locations", success: function(data){
-	        
-			data.unshift("null");
-			console.log(data);
-			for(var i=1; i<data.length; i++){
-				
-				
-				console.log(i);
-				var point = new google.maps.LatLng(
-	              parseFloat(data[i]["position_latitude"]),
-	              parseFloat(data[i]["position_longitude"]));
-	        
-	         	var marker = new google.maps.Marker({
-	            	map: map,
-	            	position: point
-	          	});
-				
-				var locatie_id = data[i]["id"];
-				//console.log(locatie_id);
-				//var j = i+1;
-				
-				//console.log(j);
-				$.ajax({url: root+"/kaart/" + locatie_id + "/api/get_locations", success: function(data2){
-				
-				
-				console.log(data2);
-				console.log(data2[0]["pivot"]["locatie_id"]);
-				if(locatie_id == data2[0]["pivot"]["locatie_id"]){
+			  
+			  if(data!=''){
+		        
+				data.unshift("null");
+				console.log(data);
+				for(var i=1; i<data.length; i++){
 					
-					console.log("ik ben binnen");
-					console.log(data2[0]["titel"]);
-					var html = data2[0]["titel"];
 					
-					var infowindow = new google.maps.InfoWindow;
-					bindInfoWindow(marker, map, infowindow, html);
+					console.log(i);
+					var point = new google.maps.LatLng(
+		              parseFloat(data[i]["position_latitude"]),
+		              parseFloat(data[i]["position_longitude"]));
+		        
+		         	var marker = new google.maps.Marker({
+		            	map: map,
+		            	position: point
+		          	});
+					
+					var locatie_id = data[i]["id"];
+					//console.log(locatie_id);
+					//var j = i+1;
+					
+					//console.log(j);
+					$.ajax({url: root+"/kaart/" + locatie_id + "/api/get_locations", success: function(data2){
+					
+					
+					console.log(data2);
+					console.log(data2[0]["pivot"]["locatie_id"]);
+					if(locatie_id == data2[0]["pivot"]["locatie_id"]){
+						
+						console.log("ik ben binnen");
+						console.log(data2[0]["titel"]);
+						var html = data2[0]["titel"];
+						
+						var infowindow = new google.maps.InfoWindow;
+						bindInfoWindow(marker, map, infowindow, html);
+						
+					}
+					
+					}});
+					
+					
+				  	
+
 					
 				}
-				
-				}});
-				
-				
-			  	
-
-				
+			}else{
+				console.log("geen locaties");
 			}
 	    }});
 	      
