e2e3d832da8c33f2abd46e63f86460fc209d9931
hasan <hasan.gurbuz@student.kdg.be>
2016-04-27 14:10:09 +0200

    homepage accordion remove

--- antwerpen_project/resources/views/home.blade.php
+++ antwerpen_project/resources/views/home.blade.php
@@ -6,7 +6,7 @@
 
 	<div id="editable">
 		<h4 id="add_project" class="nieuw_project" contenteditable='true'>Pas mij aan om een nieuw project aan te maken</h4>
-	</div>
+	</div> <!-- END DIV EDITABLE -->
 	
 	
 
@@ -19,10 +19,10 @@
 
 			
 
-		</div>
+		</div> <!-- END DIV PROJECT -->
 		{{ Form::open(array('url' => 'projecten/add'))}}
 {{ Form::close() }}
-	</div>
+	</div> <!-- END DIV ALLE_PROJECTEN -->
 
 
 
@@ -30,7 +30,7 @@
  <div class="panel-group" id="accordion">
   <div class="panel panel-default">
     
-	
+<!--	
 	<a class="accordion-toggle" data-toggle="collapse"  data-parent="#accordion" href="#collapse1">
 		<div class="panel-heading">
 	      <h4 class="panel-title">
@@ -38,22 +38,25 @@
 	        Thema 1 <i class="fa fa-angle-double-down" aria-hidden="true"></i>
 
 	      </h4>
-	    </div>
+	    </div> 
 	</a>
+-->
     <div id="collapse1" class="panel-collapse collapse in">
       <div class="panel-body" ng-repeat="project in projects">
 	  
 	  	<h1 id="div-inline"><a href="project/@{{project.id}}/view">@{{project.titel}}</a>  </h1>
 		<div id="div-inline" class="edit_home_page"><a href="project/@{{project.id}}/edit"title="pas project aan"><i class="fa fa-pencil" aria-hidden="true"></i></a></div>
 		<div id="div-inline" class="delete_home_page"><a href="project/@{{project.id}}/delete" title="verwijder project"><i class="fa fa-trash" aria-hidden="true"></i></a></div>
-		<br><small><div id="div-inline"  class="begien_datum_home_page"><strong>Begindatum: </strong>@{{project.begin_datum}}</a></div></small>
-		<small><div id="div-inline"  class="eind_datum_home_page"><strong>Einddatum: </strong>@{{project.eind_datum}}</a></div></small>
-		<br><div id="div-inline"  class="begien_datum_home_page"><strong>Beschrijving: </strong>@{{project.beschrijving}}</a></div>
-	  <hr>
-	  </div>
+<!--		<br><small><div id="div-inline"  class="begien_datum_home_page"><strong>Begindatum: </strong>@{{project.begin_datum}}</a></div></small> -->
+<!--		<small><div id="div-inline"  class="eind_datum_home_page"><strong>Einddatum: </strong>@{{project.eind_datum}}</a></div></small> -->
+<!--		<br><div id="div-inline"  class="begien_datum_home_page"><strong>Beschrijving: </strong>@{{project.beschrijving}}</a></div> -->
+	  	<hr>
+	  </div> <!-- END DIV -->
     </div>
   </div>
+  
   <div class="panel panel-default">
+ <!--
     <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#collapse2">
 		<div class="panel-heading">
 	      <h4 class="panel-title">
@@ -85,6 +88,7 @@
       commodo consequat.</div>
     </div>
   </div>
+-->
 </div> 
 
 
