7525b4f35e20e2ee53c8b3d3251a02fc6e159f11
hasan <hasan.gurbuz@student.kdg.be>
2016-04-27 11:50:21 +0200

    script file link

--- antwerpen_project/resources/views/layout.blade.php
+++ antwerpen_project/resources/views/layout.blade.php
@@ -19,6 +19,7 @@
 	<script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
 	<script  type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/angularjs/1.5.5/angular.min.js"></script>
 	<script src="{{Request::root()}}/js/base.js"></script>
+	<script src="{{Request::root()}}/js/script.js"></script>
 	<script src="http://maps.google.com/maps/api/js?sensor=false"
             type="text/javascript"></script>
 	
