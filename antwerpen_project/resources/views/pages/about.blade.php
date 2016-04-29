dc4403a175cacc59fc37a1037eff6b1cda7785b4
Kirsten Verhulst <Kirsten Verhulst>
2016-04-29 14:03:07 +0200

    branch cleaned up

--- antwerpen_project/resources/views/pages/about.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layout')
-@section('content')
-<h1>about page come here</h1>
-@stop
-    
--- antwerpen_project/resources/views/show_project.blade.php
+++
@@ -1,9 +0,0 @@
-
-@extends('layout')
-@section('content')
- 
- <h1>show project</h1>
-
-<?php print_r($project_data);?>
-
-@stop
