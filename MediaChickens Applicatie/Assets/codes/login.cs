ed55b92b9f795d328989f93303524ac41b4e354c
LLeirs <laure.leirs@student.kdg.be>
2016-05-23 22:30:17 +0200

    login added

---
+++ MediaChickens Applicatie/Assets/codes/login.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using LitJson;
+
+public class login : MonoBehaviour {
+
+    public Text txtEmail;
+    public Text txtPassWord;
+    public Text txtError;
+    public Text txtTitle;
+    public InputField inputEmail;
+    public InputField inputPassword;
+    public Button btnLogin;
+    string email;
+    string passWord;
+    string stringError;
+    public Image background;
+    public databaseConnection scriptDatabaseConnection;
+
+    string url = "http://mediachickens.multimediatechnology.be/unity/login";
+    WWWForm form;
+    WWW www;
+    void Start()
+    {
+        txtError.gameObject.SetActive(true);
+        txtPassWord.gameObject.SetActive(true);
+        txtEmail.gameObject.SetActive(true);
+        background.gameObject.SetActive(true);
+        txtTitle.text = "Log je hier in";
+        txtTitle.gameObject.SetActive(true);
+        inputEmail.gameObject.SetActive(true);
+        inputPassword.gameObject.SetActive(true);
+        btnLogin.gameObject.SetActive(true);
+        scriptDatabaseConnection = GetComponent<databaseConnection>();
+    }
+
+    private void checkUser()
+    {
+        form = new WWWForm();
+        form.AddField("email", email);
+        form.AddField("password", passWord);
+        www = new WWW(url, form);
+        StartCoroutine(WaitForRequest(www));
+    }
+
+    IEnumerator WaitForRequest(WWW www)
+    {
+        Debug.Log("stil working before return");
+        yield return www;
+
+        // check for errors
+        if (www.error == null)
+        {
+
+            Debug.Log("WWW Ok!: " + www.text);
+            JsonData dataProjects = JsonMapper.ToObject(www.text);
+            if (dataProjects["success"].ToString() == "True")
+            {
+                txtError.gameObject.SetActive(false);
+                txtPassWord.gameObject.SetActive(false);
+                txtEmail.gameObject.SetActive(false);
+                background.gameObject.SetActive(false);
+                txtTitle.gameObject.SetActive(false);
+                inputEmail.gameObject.SetActive(false);
+                inputPassword.gameObject.SetActive(false);
+                btnLogin.gameObject.SetActive(false);
+                scriptDatabaseConnection.isChoosing = true;
+            }
+            else
+            {
+                Debug.Log(dataProjects["User"]);
+                if (dataProjects["User"].ToString() == "Gebruiker bestaat niet")
+                {
+                    stringError = "E-mail of wachtwoord is fout";
+                }
+                else { 
+                stringError = "";
+                for (int i = 0; i < dataProjects["errors"].Count; i++)
+                {
+                    for (int j = 0; j < dataProjects["errors"][i].Count; j++)
+                    {
+                        stringError += dataProjects["errors"][i][j] + "\n";
+                    }
+                }
+                }
+                txtError.text = stringError;
+            }
+        }
+        else
+        {
+            Debug.Log("WWW Error: " + www.error);
+        }
+    }
+    public void btnLoginClicked()
+    {
+        email = txtEmail.text;
+        passWord = txtPassWord.text;
+        checkUser();
+    }
+
+}
